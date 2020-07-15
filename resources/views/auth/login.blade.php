4837fc5483738fe8fa9f84a8f4898ea1a421e870
Sagar Chhetri <sagar.chhetri@student.trinity.edu.np>
2020-07-15 14:48:45 +0545

    Update Site Demo V0.5.1
    Update Login Form (Both User/Admin)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="offset-md-5"><h1>{{ __('User Login') }}</h1></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
